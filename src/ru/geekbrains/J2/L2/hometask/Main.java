86d2f1f7db00af367c779c34c02b1399b352c355
teriberka <box4trash@inbox.ru>
2020-05-19 15:21:36 +0300

    commit 2

--- src/ru/geekbrains/J2/L2/hometask/Main.java
+++ src/ru/geekbrains/J2/L2/hometask/Main.java
@@ -23,6 +23,45 @@ package ru.geekbrains.J2.L2.hometask;
 
 public class Main {
     public static void main(String[] args){
+        String inputString = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+
+        String[][] someArray;
+
+        someArray = parseString(inputString);
+        printArray(someArray);
 
     }
+
+    public static String[][] parseString(String inputString) {
+        String[] subArray1;
+        String[] subArray2;
+
+        subArray1 = inputString.split("\n");
+
+        int n = subArray1.length;
+
+        String[][] resultArray = new String[n][n];
+
+        for (int i = 0; i < subArray1.length; i++) {
+//            System.out.println(subArray1[i]);
+            subArray2 = subArray1[i].split(" ");
+            for (int j = 0; j < subArray2.length; j++) {
+//                System.out.println(subArray2[j]);
+                resultArray[i][j] = subArray2[j];
+            }
+        }
+
+        return resultArray;
+    }
+
+
+    public static void printArray(String[][] incomeArray){
+        for (int i = 0; i < incomeArray.length; i++){
+            for(int j = 0; j < incomeArray.length; j++){
+                System.out.print(incomeArray[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
 }
